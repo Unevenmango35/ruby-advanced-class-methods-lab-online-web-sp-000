2034f36891decd5c73fbe0d261cb4cd7bf62f23b
Rosie Choi <rosiechoimanso@gmail.com>
2020-10-08 02:50:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,12 +29,8 @@ def self.create_by_name(name)
 end
 
 def self.find_by_name(name)
-  @@all.detect { |song|
-      song.name == name
-    }
+    self.all.detect { |song| song.name == name }
   end
-end
-end
 
 def self.find_or_create_by_name(name)
   self.find_by_name(name) || self.create_by_name(name)
